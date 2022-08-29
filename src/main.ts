8604b5859789e3a045b2399c1dce27b2b108f544
Jose Lopes <josluis.lopes@gmail.com>
2022-08-29 11:45:15 +0100

    Added Telegram client
    Added Telegram client

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { TelegramClient } from './clients/telegramClient';
 import { TwitterClient } from './clients/twitterClient';
 
 require('dotenv').config()
@@ -5,8 +6,13 @@ require('dotenv').config()
 async function main() {
     console.log('Starting up...');    
 
+    console.log('Starting twitter client...');
     const twitterClient = new TwitterClient();
     twitterClient.start();
+
+    console.log('Starting telegram client...');
+    const telegramClient = new TelegramClient(process.env['TELEGRAM_BOT_TOKEN'] ?? '');
+    telegramClient.start();
 }
 
 main()
