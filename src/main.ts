2cbd4e3c1ded6e4c0a662300c01fb046c8c17bbf
Jose Lopes <josluis.lopes@gmail.com>
2023-06-09 12:09:11 +0100

    Attempt to fix multiple client error

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,56 @@ import { DiscordClient } from './clients/discordClient';
 
 require('dotenv').config()
 
-async function main() {
-    console.log('Starting up...');    
-
+async function startTwitterClient(): Promise<void> {
     if ("true" === process.env['TWITTER_ENABLED']) {
         console.log('Starting twitter client...');
         const twitterClient = new TwitterClient();
-        twitterClient.start();
+        try {
+            await twitterClient.start();
+        } catch (error) {
+            console.error('Error starting twitter client: ' + error);
+            // Retry in 5 minutes
+            setTimeout(startTwitterClient, 5 * 60 * 1000);
+        }
     }
-    
+}
+
+async function startTelegramClient(): Promise<void> {
     if ("true" === process.env['TELEGRAM_ENABLED']) {
         console.log('Starting telegram client...');
         const telegramClient = new TelegramClient(process.env['TELEGRAM_BOT_TOKEN'] ?? '');
-        telegramClient.start();
+        try {
+            await telegramClient.start();
+        } catch (error) {
+            console.error('Error starting telegram client: ' + error);
+            // Retry in 1 minutes
+            setTimeout(startTelegramClient, 60 * 1000);
+        }
     }
+}
 
+async function startDiscordClient(): Promise<void> {
     if ("true" === process.env['DISCORD_ENABLED']) {
         console.log('Starting discord client...');
         const discordClient = new DiscordClient(process.env['DISCORD_CLIENT_ID'] ?? '', process.env['DISCORD_BOT_TOKEN'] ?? '');
-        discordClient.start();
+        try {
+            await discordClient.start();
+        } catch (error) {
+            console.error('Error starting discord client: ' + error);
+            // Retry in 1 minutes
+            setTimeout(startDiscordClient, 60 * 1000);
+        }
     }
+}
+
+async function main() {
+    console.log('Starting up...');    
+
+    Promise.all([
+        startTwitterClient(),
+        startTelegramClient(),
+        startDiscordClient()
+    ])
 }
 
 main()
